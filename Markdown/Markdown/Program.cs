c9ccc93e66e3b03c472d96c3262f54e551e55855
Dodo888 <darkdodo888@gmail.com>
2015-11-01 01:12:46 +0500

    Classes design
    Classes with only signatures of methods.

---
+++ Markdown/Markdown/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class FileParser
+    {
+        public static string ReadFile(string path)
+        {
+            return "";
+        }
+
+        public static void writeFile(string path)
+        {
+        }
+    }
+
+    class MarkdownMaker
+    {
+        private Dictionary<string, Tuple<string>> tagForSymbol;
+
+        public string findMarkdownSymbol()
+        {
+            var tag = "";
+            return tag;
+        }
+
+        public string replaceMarkdownSequence(bool isOpening, int sequenceLength, string tag)
+        {
+            return "";
+        }
+
+        public bool hasEscapedCharacter(int sequenceLength)
+        {
+            return true;
+        }
+
+        public bool hasNumbers(int sequenceLength)
+        {
+            return true;
+        }
+    }
+}
